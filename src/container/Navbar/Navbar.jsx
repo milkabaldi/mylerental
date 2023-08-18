a8e11b406e4bf484b6445921aae079b15c4e2ae5
milkabald <justmainc8@gmail.com>
2023-08-18 22:15:36 +0300

    Navbar

--- src/container/Navbar/Navbar.jsx
+++ src/container/Navbar/Navbar.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+      <div>
+        <img src="" alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>Industries</li>
+          <li>Technology</li>
+          <li>Driver weith Myle</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div>
+        <div>
+          search
+        </div>
+        <button>Contact Sales</button>
+      </div>
+    </header>
   )
 }

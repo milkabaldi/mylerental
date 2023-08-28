c540fe60696fd67fb3b747b290ff42bcaabf8079
milkabald <justmainc8@gmail.com>
2023-08-28 21:14:02 +0300

    Hero

--- src/container/Navbar/Hero.jsx
+++ src/container/Navbar/Hero.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div> 
+      <div>Hero</div>
+      <div>Hero</div>
+      <div>Test</div></div>
+   
   )
 }
 

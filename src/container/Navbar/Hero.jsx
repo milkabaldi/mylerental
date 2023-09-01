35abe5b20630e6fb47d1b1916676b4ca39150cd7
milkabald <justmainc8@gmail.com>
2023-09-01 20:49:38 +0300

    test

--- src/container/Navbar/Hero.jsx
+++ src/container/Navbar/Hero.jsx
@@ -6,6 +6,9 @@ const Hero = () => {
       <div className={`${styles.boxWidth} `}>
         <div>test</div>
         <div>test</div>
+        <div>test</div>
+        <h2>Mytest</h2>
+        <div>test</div>
       </div>
 
     </section>

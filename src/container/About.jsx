a80d7731b0b21af6cd6fe6ed5bb5c6c2f16210bb
milkabald <justmainc8@gmail.com>
2023-09-26 18:43:32 +0300

    Changes

--- src/container/About.jsx
+++ src/container/About.jsx
@@ -3,51 +3,8 @@ import React from 'react'
 export const About = () => {
   return (
     <section>
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-      <div className='container'>
-          <h1>About Us</h1>
-          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
-          </p>
-          little test
-      </div>
-      <div className='container'>
-          <h1>About Us</h1>
+      <div className='container about us'>
+          <h1>About</h1>
           <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
           </p>
           little test

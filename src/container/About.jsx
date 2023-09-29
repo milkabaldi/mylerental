7183227030ae7730b122fa10884491a4d1301ba0
milkabald <justmainc8@gmail.com>
2023-09-29 12:02:16 +0300

    test

--- src/container/About.jsx
+++ src/container/About.jsx
@@ -7,7 +7,10 @@ export const About = () => {
           <h1>About</h1>
           <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
           </p>
-          <h1>Price</h1>
+          <h2>Price</h1>
+          <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
+          </p>
+          <h2>Our mission</h1>
           <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
           </p>
       </div>

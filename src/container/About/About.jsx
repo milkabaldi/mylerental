144165be1d0b07c64ea9feb0fff8f72cc4ebd765
milkabald <justmainc8@gmail.com>
2024-01-12 17:00:20 +0200

    Today

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <section>
       <div className={`${styles.boxWidth} m-auto`}>
-          <h1>About Us </h1>
+          <h1>About Us Maybe today </h1>
           <p>Being an industry leader, we have recognized the difficulties encountered by our private use clients nationwide in need of a wheelchair accessible vehicle rental. We set out to provide comfortable, reliable options for you, your family, and your travel needs. We help alleviate those stresses with our wide range of ADA compliant vehicles and unique rental options.
           </p>aaa
       </div>

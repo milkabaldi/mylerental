75b75c1ab0073a4ad516d3598b53de47280069ff
milkabald <justmainc8@gmail.com>
2024-01-13 23:14:33 +0200

    Today

--- src/container/Navbar/Hero.jsx
+++ src/container/Navbar/Hero.jsx
@@ -5,19 +5,15 @@ import images from '../../constants/images.js'
 const Hero = () => {
   return (
     <section> 
-      <div className={`${styles.boxWidth} m-auto`}>
+      <div className={`${styles.boxWidth} m-auto flex items-center`}>
         <div>
             <span>Car Rental</span>
             <h1>Rent ADA Compliant Wheelchair Accessible Vehicle’s Nationwide </h1>
             <p>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
-            <p>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
-            <p>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
-            <p>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
-            <p>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
             <button>Inquire Now</button>
         </div>
         <figure>
-          <img src="" alt="Cars" />
+          <img src={images.HeroCar} alt="Cars" />
         </figure>
       </div>
 

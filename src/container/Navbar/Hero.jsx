1ef3eb61e5220d8c976ef4f7faa3f5bc0e6ca41e
milkabald <justmainc8@gmail.com>
2024-01-14 11:09:58 +0200

    CSS styles

--- src/container/Navbar/Hero.jsx
+++ src/container/Navbar/Hero.jsx
@@ -4,12 +4,12 @@ import images from '../../constants/images.js'
 
 const Hero = () => {
   return (
-    <section> 
+    <section className='mt-16'> 
       <div className={`${styles.boxWidth} m-auto flex items-center`}>
         <div>
-            <span>Car Rental</span>
-            <h1>Rent ADA Compliant Wheelchair Accessible Vehicle’s Nationwide </h1>
-            <p>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
+            <span className='text-base'>Car Rental</span>
+            <h1 className='text-4xl'>Rent ADA Compliant Wheelchair Accessible Vehicle’s Nationwide </h1>
+            <p className='text-sm'>Various ADA compliant wheelchair accessible vehicles available for rent nationwide. </p>
             <button>Inquire Now</button>
         </div>
         <figure>

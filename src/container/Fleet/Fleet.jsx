39c9552b2d068e183301956b0441ec82c8fcb224
milkabald <justmainc8@gmail.com>
2024-01-17 21:44:46 +0200

    Test

--- src/container/Fleet/Fleet.jsx
+++ src/container/Fleet/Fleet.jsx
@@ -7,7 +7,7 @@ const Fleet = () => {
     return (
       <section className='mb-32 mt-32'>
         <div className={`${styles.boxWidth} m-auto`}>
-            <h2>Fleet Inventory </h2>
+            <h2>Fleet Inventory Test</h2>
             <p>We offer ADA compliant wheelchair accessible vehicles with different builds to accommodate groups of various sizes. Our fleet includes the Chrysler Pacifica and Toyota SiennaTravel with ease and comfort!</p>
         </div>
       </section>

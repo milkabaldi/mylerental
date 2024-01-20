a39d87342bba9dafc32c006db2f03aa6c2299b07
milkabald <justmainc8@gmail.com>
2024-01-20 12:19:37 +0200

    Fleet section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,9 @@
 import Logo from '../assets/myle-logo.png'
 import HeroCar from '../assets/HeroCar.png'
+import CarSlider from '../assets/CarSlider.png'
 
 export default {
     Logo,
-    HeroCar
+    HeroCar,
+    CarSlider,
 }
--- src/container/Fleet/Fleet.jsx
+++ src/container/Fleet/Fleet.jsx
@@ -6,9 +6,12 @@ import images from '../../constants/images.js'
 const Fleet = () => {
     return (
       <section className='mb-32 mt-32'>
-        <div className={`${styles.boxWidth} m-auto`}>
-            <h2>Fleet Inventory</h2>
-            <p>We offer ADA compliant wheelchair accessible vehicles with different builds to accommodate groups of various sizes. Our fleet includes the Chrysler Pacifica and Toyota SiennaTravel with ease and comfort!</p>
+        <div className={`${styles.boxWidth} m-auto flex flex-col items-center justify-between text-center`}>
+            <h2 className='text-4xl mb-6'>Fleet Inventory</h2>
+            <p className='text-sm'>We offer ADA compliant wheelchair accessible vehicles with different builds to accommodate groups of various sizes. Our fleet includes the Chrysler Pacifica and Toyota SiennaTravel with ease and comfort!</p>
+            <div className='mt-10'>
+              <img src={images.CarSlider} alt="Cars" />
+            </div>
         </div>
       </section>
     )

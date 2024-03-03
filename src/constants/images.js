2bfd438ebf4360f9031acbd551b83f8d2657a788
milkabald <justmainc8@gmail.com>
2024-03-03 15:53:10 +0200

    Image

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,35 @@
 import Logo from '../assets/myle-logo.png'
 import HeroCar from '../assets/HeroCar.png'
 import CarSlider from '../assets/CarSlider.png'
+import FooterCar  from '../assets/footer-car.png'
+import Mao  from '../assets/map.png'
+import Component16  from '../assets/Component16.png'
+import Calendar  from '../assets/calendar.png'
+import Chrysler  from '../assets/chrysler.png'
+import Wheel  from '../assets/wheelchair.png'
+import Secure  from '../assets/secure.png'
+import Auto  from '../assets/Auto.png'
+import Component11  from '../assets/Component11.png'
+
+
+
+
+
 
 export default {
     Logo,
     HeroCar,
     CarSlider,
+    FooterCar,
+    Mao,
+    Component16,
+    Calendar,
+    Chrysler,
+    Wheel,
+    Secure,
+    Auto,
+    Component11,
+
+
+
 }
--- src/container/Fleet/Fleet.jsx
+++ src/container/Fleet/Fleet.jsx
@@ -7,7 +7,7 @@ const Fleet = () => {
     return (
       <section className='mb-32 mt-32'>
         <div className={`${styles.boxWidth} m-auto flex flex-col items-center justify-between text-center`}>
-            <h2 className='text-4xl mb-6'>Fleet Inventory 111</h2>
+            <h2 className='text-4xl mb-6'>Fleet Inventory</h2>
             <p className='text-sm'>We offer ADA compliant wheelchair accessible vehicles with different builds to accommodate groups of various sizes. Our fleet includes the Chrysler Pacifica and Toyota SiennaTravel with ease and comfort!</p>
             <div className='mt-10'>
               <img src={images.CarSlider} alt="Cars" />

38280deccf949622c84c45a41c277285e869052c
milkabald <justmainc8@gmail.com>
2024-07-25 19:52:14 +0300

    Commit

--- src/container/Benefits/Benefits.jsx
+++ src/container/Benefits/Benefits.jsx
@@ -19,7 +19,7 @@ const Benefits = () => {
                     </ul>
                 </div>
                 <div className='benImg'>
-                    <img src={images.benCarOne} alt="Carsss" />
+                    <img src={images.benCarOne} alt="Car ss" />
                 </div>
             </div>
         </div>

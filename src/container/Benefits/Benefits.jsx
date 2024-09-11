aad3b64396c8116fdd56268a8beba6b9e458d65e
milkabaldi <justmainc8@gmail.com>
2024-09-11 21:45:48 +0300

    Null

--- src/container/Benefits/Benefits.jsx
+++ src/container/Benefits/Benefits.jsx
@@ -19,7 +19,7 @@ const Benefits = () => {
                     </ul>
                 </div>
                 <div className='benImg'>
-                    <img src={images.benCarOne} alt="Car new 1" />
+                    <img src={images.benCarOne} alt="Car new 2" />
                 </div>
             </div>
         </div>

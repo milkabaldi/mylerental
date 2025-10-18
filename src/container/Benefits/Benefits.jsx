1f72e258639ab34e4e039a8eeba36ae3eec9e279
milkabaldi <justmainc8@gmail.com>
2025-10-18 23:17:24 +0300

    Test

--- src/container/Benefits/Benefits.jsx
+++ src/container/Benefits/Benefits.jsx
@@ -19,7 +19,7 @@ const Benefits = () => {
                     </ul>
                 </div>
                 <div className='benImg'>
-                    <img src={images.benCarOne} alt="Slider Cars Imagesss" />
+                    <img src={images.benCarOne} alt="Slider Cars Imagessss" />
                 </div>
             </div>
         </div>
